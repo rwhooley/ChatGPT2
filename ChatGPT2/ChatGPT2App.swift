374d869e95f38c01f175d8f658fb2009c7c6ff74
rwhooley <ryan.whooley@gmail.com>
2024-08-23 18:14:21 -0400

    Initial Commit

---
+++ ChatGPT2/ChatGPT2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT2App.swift
+//  ChatGPT2
+//
+//  Created by Ryan Whooley on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
